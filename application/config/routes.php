1583b50afa10b4d56f409a16f31371de72419ae3
ajesbandieth <ajesbandieth@gmail.com>
2026-04-08 14:49:04 +0700

    Implement User Registration and CRUD API (Issue #3)

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// API Routes
+$route['api/users']['post'] = 'api/UserController/register';
+$route['api/users']['get'] = 'api/UserController/index';
+$route['api/users/(:num)']['get'] = 'api/UserController/show/$1';
+$route['api/users/(:num)']['put'] = 'api/UserController/update/$1';
+$route['api/users/(:num)']['delete'] = 'api/UserController/destroy/$1';
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,26 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model {
 
+    private $table = 'users';
+
     public function __construct() {
         parent::__construct();
         $this->load->database();
     }
 
-    /**
-     * Get all users
-     * @return array
-     */
-    public function get_all_users() {
-        $query = $this->db->get('users');
+    public function is_email_registered($email) {
+        $query = $this->db->get_where($this->table, array('email' => $email));
+        return $query->num_rows() > 0;
+    }
+
+    public function create($data) {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+
+    public function get_all() {
+        $query = $this->db->get($this->table);
         return $query->result_array();
     }
 
-    /**
-     * Create a new user
-     * @param array $data
-     * @return bool
-     */
-    public function create_user($data) {
-        return $this->db->insert('users', $data);
+    public function get_by_id($id) {
+        $query = $this->db->get_where($this->table, array('id' => $id));
+        return $query->row_array();
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table, $data);
+    }
+
+    public function delete($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->table);
     }
 }
